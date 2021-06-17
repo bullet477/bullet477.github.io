1046b5fd89acf33a4e116f7185109ce4df6da140
Jakub Sekanina <jakub.sek@seznam.cz>
2021-06-17 18:46:21 +0200

    Initial commit

---
+++ assets/Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        //Read the package.json (optional)
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Metadata.
+        meta: {
+            basePath: '../',
+            srcPath: '../sass/',
+            deployPath: '../css/'
+        },
+
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ',
+
+        // Task configuration.
+        sass: {
+            dist: {
+                files: {
+                    'css/main.css' : 'sass/main.scss'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['sass/**/*.scss'],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['sass']);
+};
